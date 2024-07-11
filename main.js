0d31eaf7622bcd56be09bb65269519b8f0090b18
khadijabayramova <safarlikhadija98@gmail.com>
2024-07-11 17:59:24 +0400

    created to do list by using html css and js

---
+++ main.js
@@ -0,0 +1,29 @@
+document.querySelector(".push").onclick = function () {
+  if (document.querySelector(".newtask input").value.length == 0) {
+    alert("Please enter a task.");
+  } else {
+    document.querySelector(
+      ".tasks"
+    ).innerHTML += `<div class="task"> <span id="taskname">
+      ${document.querySelector(".newtask input").value}
+      </span>
+      <button class="delete">
+      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+      </button>
+      </div>`;
+
+    let current_tasks = document.querySelectorAll(".delete");
+    for (let i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+    let tasks = document.querySelectorAll(".tasks");
+    for (let i = 0; i < tasks.length; i++) {
+      tasks[i].onclick = function () {
+        this.classList.toggle("completed");
+      };
+    }
+    document.querySelector(".newtask input").value = "";
+  }
+};
